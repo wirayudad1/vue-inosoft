ec19237053ed1bf2e7c13a2467c997ed5c22e693
Wirayuda Dewandhana <wira@mit.id>
2023-03-25 21:38:25 +0700

    Update: Layout Draft Form

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -6,42 +6,155 @@
  * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('Testing.view.main.Main', {
-    extend: 'Ext.container.Container',
-    requires: [
-        'Testing.view.main.MainController',
-        'Testing.view.main.MainModel'
-    ],
+    extend: 'Ext.panel.Panel',
+    initComponent () {
+        let me=this;
+        Ext.apply(this, {
+            layout: 'fit',
+            items: [
+                this.createList()
+            ],
+            // bbar:[
+            //     '->',
+            //
+            // ]
+           
+        })
 
-    xtype: 'app-main',
-
-    controller: 'main',
-    viewModel: {
-        type: 'main'
+        this.callParent(arguments);
     },
+    createList(){
+        let me=this
+        let storeCombobox= Ext.create('Ext.data.Store', {
+            fields:['id','name'],
+            data : [
+                {id: '1',    name: 'Tracking Instruction'},
+                {id: '2',    name: 'Logistic Instruction'},
 
-    layout: {
-        type: 'border'
+            ]
+        });
+        let panel=Ext.create('Ext.panel.Panel',{
+            title:`<b style="color:#545454">3rd Party Instruction</b>`,
+            width: '100%',
+            layout:{
+                type:'hbox'
+            },
+            cls:'headerPanel',
+            items:[
+                // {
+                //     xtype:'panel',
+                //     layout:{
+                //         type:'vbox'
+                //     },
+                //     items:[
+                //         {
+
+                //         }
+                //     ]
+                // },
+                {
+                    xtype:'combobox',
+                    store:storeCombobox,
+                    margin:8,
+                    padding:8,
+                    value:'2',
+                    displayField:'name',
+                    valueField:'id'
+                    
+                },
+                me.createForm()
+            ]
+        })
+        return panel
     },
+    createForm(){
+        let me=this;
+        let storeCombobox= Ext.create('Ext.data.Store', {
+            fields:['id','name'],
+            data : [
+                {id: '1',    name: 'Tracking Instruction'},
+                {id: '2',    name: 'Logistic Instruction'},
 
-    items: [{
-        xtype: 'panel',
-        bind: {
-            title: '{name}'
-        },
-        region: 'west',
-        html: '<ul><li>This area is commonly used for navigation, for example, using a "tree" component.</li></ul>',
-        width: 250,
-        split: true,
-        tbar: [{
-            text: 'Button',
-            handler: 'onClickButton'
-        }]
-    },{
-        region: 'center',
-        xtype: 'tabpanel',
-        items:[{
-            title: 'Tab 1',
-            html: '<h2>Content appropriate for the current navigation.</h2>'
-        }]
-    }]
+            ]
+        });
+        let form=Ext.create('Ext.form.Panel',{
+            title:`<div style="color:#545454;width:120px; background-color:#E5E5E5;text-align:center; font-weight:bold;border-radius:10px;padding:4px;">Draft</div>`,
+            layout:{
+                type:'form'
+            },
+            items:[
+                {
+                    xtype:'panel',
+                    layout:{
+                        type:'hbox'
+                    },
+                    items:[
+                        {
+                            xtype:'combobox',
+                            store:storeCombobox,
+                            labelAlign:'top',
+                            fieldLabel:'Assigned Vendor',
+                            margin:8,
+                            padding:8,
+                            value:'2',
+                            displayField:'name',
+                            valueField:'id'
+                            
+                        },
+                        {
+                            xtype:'textfield',
+                            labelAlign:'top',
+                            fieldLabel:'Attention Of',
+                            margin:8,
+                            padding:8,
+                            value:'John Smith ',
+                            
+                        },
+                        {
+                            xtype:'textfield',
+                            labelAlign:'top',
+                            fieldLabel:'Quotation No',
+                            margin:8,
+                            padding:8,
+                            value:'ABC-CDE 21',
+                            
+                        },
+                        {
+                            xtype:'combobox',
+                            store:storeCombobox,
+                            labelAlign:'top',
+                            fieldLabel:'Invoice To',
+                            margin:8,
+                            padding:8,
+                            value:'2',
+                            displayField:'name',
+                            valueField:'id'
+                            
+                        },
+                    ]
+                },
+                {
+                    xtype:'panel',
+                    layout:{
+                        type:'hbox'
+                    },
+                    items:[
+                        
+                        {
+                            xtype:'textfield',
+                            labelAlign:'top',
+                            width:'19%',
+                            fieldLabel:'Vendor Address',
+                            margin:8,
+                            padding:8,
+                            value:'ABC-CDE 21',
+                            
+                        },
+                    ]
+                }
+            ]
+        })
+        return form
+
+    }
 });
